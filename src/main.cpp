fb1e70f07898b0cda9107fa59c1aca9fed2f154f
CarloAncri <carlo.ancri@studenti.unimi.it>
2026-02-26 10:07:51 +0100

    link altre librerie

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <vector>
 
+#ifdef _WIN32
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+
 #include "../oscpkt.hh"
 
+#define XR18_IP "192.168.1.xxx"
+#define XR18_PORT 10024
+
+//* PROGRAM HEAD
 int main(int argc, char **argv[])
 {
 }
